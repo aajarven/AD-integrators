d6c129b0471a346afd81dd21b56661b85a25bf77
aajarven <anni.jarvenpaa@gmail.com>
2017-04-26 22:40:30 +0300

    teoriassa valmis

--- leapfrog.c
+++ leapfrog.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "arrayUtils.h"
 #include "physUtils.h"
+#include "fileio.h"
 
 /*
  * Integrates 3D system with initial positions and velocites of masses
@@ -17,24 +18,38 @@
  *  nBodies     Integer, number of particles in simulation
  *  dimensions  Integer, number of dimensions in simulation
  *  dt          Double, length of the time step in seconds
+ *  endtime     Double, time at which the simulation terminates
  *  outFreq     Integer giving number of simulation steps between dumping
  *              otput to file.
  *  output      Pointer to file that is used to save outputs
  *
  */
 void leapfrog(double *masses, double *positions, double *velocities,
-        int nBodies, int dimensions, double dt, int outFreq, FILE *output){
+        int nBodies, int dimensions, double dt, double endtime, int outFreq, FILE *output){
 
-    
+
     // create a copy of initial positions and velocities to avoid modifying
     // the original arrays
     double *pos = dArrCopy(positions, nBodies*dimensions);
     double *vel = dArrCopy(velocites, nBodies*dimensions);
-
-
-    // Caculate gravitational accelerations from other particles
-    double* a = calculateAccelerations(masses, pos, nBodies, dimensions);
-
+    double *a;
+    double time = 0;
     int loopNum = 0;
-    //TODO loopstuff
+
+    while (time < endtime){
+        time += dt;
+        loopNum++;
+
+        a = calculateAccelerations(masses, pos, nBodies, dimensions);
+        kick(vel, a, dt/2.0, nBodies, dimensions);
+        
+        drift(pos, vel, dt, nBodies, dimensions);
+        
+        a = calculateAccelerations(masses, pos, nBodies, dimensions);
+        kick(vel, a, dt/2.0, nBodies, dimensions);
+
+        if(loopNum%outFreq = 0){
+            dumpSim(output, pos, vel, nBodies, dimensions);
+        }
+    }
 }
