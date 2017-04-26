356689a132ea7093f87e1cffc8de2df79d0a4a62
aajarven <anni.jarvenpaa@gmail.com>
2017-04-26 23:19:53 +0300

    testaa luku

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "fileio.h"
+#include "leapfrog.h"
+
+
+void main(){
+    FILE *in;
+    in = fopen("input/input_precise.dat", "r");
+
+    double *positions = malloc(6*3*sizeof(double));
+    double *velocities = malloc(6*3*sizeof(double));
+    double *masses = malloc(6*sizeof(double));
+
+    readInitialConditions(in, positions, velocities, masses);
+}
