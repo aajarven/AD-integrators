918b25e959943b21d6629680d7fb4f38f02b5946
aajarven <anni.jarvenpaa@gmail.com>
2017-04-28 00:54:44 +0300

    outputteja ja plotteja. keskitys massakeskipisteeseen puuttuu vielä

--- plottaus/xyradat.m
+++ plottaus/xyradat.m
@@ -1,7 +1,22 @@
 colors = [204 0 153; 255 0 0; 255 204 0; 51 204 204; 0 0 204; 0 0 0]/255;
-colorsRepeated = colors(repmat(1:size(colors,1), length(index)/6,1),:);
+colorsRepeated = ones(length(index), 3);
+%colorsRepeated = colors(repmat(1:size(colors,1), length(index)/6,1),:);
+%colorsRepeated = repmat(colors, length(index)/6);
+%colorsRepeated = colorsRepeated(:,1:3);
 sizes = [40; 2; 2; 2; 2; 2];
-sizesRepeated = sizes(repmat(1:size(sizes,1), length(index)/6,1),:);
+sizesRepeated = ones(length(index), 1);
+%sizesRepeated = sizes(repmat(1:size(sizes,1), length(index)/6,1),:);
+%sizesRepeated = repmat(sizes, length(index)/6);
+%sizesRepeated = sizesRepeated(:,1);
+
+for i=1:length(index)
+    sizesRepeated(i) = sizes(mod(i-1, 6)+1);
+    
+    for j=1:3
+        colorsRepeated(i,j) = colors(mod(i-1,6)+1,j);
+    end
+    
+end
 
 scatter(x, y, sizesRepeated, colorsRepeated, 'filled')
 
