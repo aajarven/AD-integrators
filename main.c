c14a00f68c7aa877eab57a0366a5e651061c2aed
aajarven <anni.jarvenpaa@gmail.com>
2017-04-28 00:54:14 +0300

    parametrit komentoriviltä

--- main.c
+++ main.c
@@ -6,22 +6,36 @@
 #include "consts.h"
 
 
-void main(){
-    FILE *in;
-    in = fopen("input/input_precise.dat", "r");
+int main(int argc, char *argv[]){
 
-    double *positions = malloc(6*3*sizeof(double));
-    double *velocities = malloc(6*3*sizeof(double));
-    double *masses = malloc(6*sizeof(double));
+    if (argc<9){
+        printf("\nYou must give path to initial conditions, number of bodies in simulation, number of dimensions, length of time step (in years), length of simulation (yr), number of integration time steps between consecutive outputs to file and path to the output destination file and simulation type ('l' for leapfrog or 'r' for RK4). For example\n\tinput/input_precise.dat 6 3 0.01 250 10 output/out.dat l\nfor leapfrog simulation with 6 bodies in 3D space using timestep of 0.01 yr, running 250 years and outputting every 0.1 years, where input is read from file input/input_precise.dat and output is written to output/out.dat.\n\nNB! Erroneous arguments or nonexistent input file may cause an unhandled crash.\n\n");
+        exit(-1);
+    }
+
+    FILE *in;
+    in = fopen(argv[1], "r");
+
+    int nBodies, dimensions, outFreq;
+    double step, endTime;
+    sscanf(argv[2], "%d", &nBodies);
+    sscanf(argv[3], "%d", &dimensions);
+    sscanf(argv[4], "%lf", &step);
+    sscanf(argv[5], "%lf", &endTime);
+    sscanf(argv[6], "%d", &outFreq);
+
+    double *positions = malloc(nBodies*dimensions*sizeof(double));
+    double *velocities = malloc(nBodies*dimensions*sizeof(double));
+    double *masses = malloc(nBodies*sizeof(double));
 
     readInitialConditions(in, positions, velocities, masses);
-    dArrMultiply(velocities, dayInYr, 6*3);
-    dArrMultiply(masses, 1.0/kgInMSun, 6);
-    dArrMultiply(masses, 4*pi*pi, 6);
+    dArrMultiply(velocities, dayInYr, nBodies*dimensions);
+    dArrMultiply(masses, 1.0/kgInMSun, nBodies);
+    dArrMultiply(masses, 4*pi*pi, nBodies);
 
     FILE *out;
-    out = fopen("output/leapfrog-testi.dat", "w");
-    leapfrog(masses, positions, velocities, 6, 3, 0.01, 2, 1, out);
+    out = fopen(argv[7], "w");
+    leapfrog(masses, positions, velocities, nBodies, dimensions, step, endTime, outFreq, out);
 
     free(positions);
     free(velocities);
